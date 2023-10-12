9a39e39a2256af14bd594ff3d52fb3052b083d20
Juan Sebastian <juan.sebastianri28@gmail.com>
2023-10-12 02:11:40 -0500

    Delete style tag

--- estudiantes.php
+++ estudiantes.php
@@ -1,10 +1,5 @@
 <html>
 <head>
-        <style>
-                table, tr, th, td{
-                        border: 1px solid;
-                }
-        </style>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
