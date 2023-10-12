7127f38beb0471e0d655ea59f8d04394c3997d30
Juan Sebastian <juan.sebastianri28@gmail.com>
2023-10-12 02:09:37 -0500

    Implements Bootsrap v5.3

--- estudiantes.php
+++ estudiantes.php
@@ -1,48 +1,58 @@
 <html>
 <head>
-	<style>
-		table, tr, th, td{
-			border: 1px solid;
-		}
-	</style>
+        <style>
+                table, tr, th, td{
+                        border: 1px solid;
+                }
+        </style>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
+<div class="container">
 <?php
-	$SERVER = getenv("SERVER");
-	$USER = getenv("USER");
-	$PASS = getenv("PASS");
-	$DB = getenv("DATABASE");
-	
-	$conn = new mysqli($SERVER, $USER, $PASS, $DB);
+        $SERVER = getenv("SERVER");
+        $USER = getenv("USER");
+        $PASS = getenv("PASS");
+        $DB = getenv("DATABASE");
 
-	if($conn->connect_error){
-		die('Could not connect: '.$conn->connect_error);
-	}
+        $conn = new mysqli($SERVER, $USER, $PASS, $DB);
 
-	$query = "SELECT * FROM estudiante";
+        if($conn->connect_error){
+                die('Could not connect: '.$conn->connect_error);
+        }
 
-	$result = $conn->query($query);
+        $query = "SELECT * FROM estudiante";
 
-	if($result->num_rows > 0){
-		echo "<table>";
-		echo "<tr><th>ID</th><th>Nombre</th><th>Apellido</th><th>Correo</th><th>Cargo</th><th>Ciudad</th></tr>";
-		
-		while($row = $result->fetch_assoc()){
-			echo "<tr>";
-			echo "<td>".$row["id"]."</td>";	
-			echo "<td>".$row["name"]."</td>";
-			echo "<td>".$row["last_name"]."</td>";
-			echo "<td>".$row["email"]."</td>";
-			echo "<td>".$row["position"]."</td>";
-			echo "<td>".$row["city"]."</td>";
-			echo "</tr>";	
-		}
-		echo "</table>";
-	}else{
-		echo "No se encontraron resultados";
-	}
+        $result = $conn->query($query);
 
-	$conn->close();
+        if($result->num_rows > 0){
+                echo '<table class="table table-bordered">';
+
+                echo '<thead>';
+                $th = '<th scope="col">';
+                echo '<tr class="table-secondary">'.$th."ID</th>".$th."Nombre</th>".$th."Apellido</th>".$th."Correo</th>".$th."Cargo</th>".$th."Ciudad</th></tr>";
+                echo '</thead>';
+
+                echo '<tbody class="table-group-divider">';
+
+                while($row = $result->fetch_assoc()){
+                        echo "<tr>";
+                        echo "<td>".$row["id"]."</td>";
+                        echo "<td>".$row["name"]."</td>";
+                        echo "<td>".$row["last_name"]."</td>";
+                        echo "<td>".$row["email"]."</td>";
+                        echo "<td>".$row["position"]."</td>";
+                        echo "<td>".$row["city"]."</td>";
+                        echo "</tr>";
+                }
+                echo '</tbody>';
+                echo "</table>";
+        }else{
+                echo "No se encontraron resultados";
+        }
+
+        $conn->close();
 ?>
+</div>
 </body>
 </html>
